f93edbdd58630b0f7a0c276acf91fa292176538f
ofekefrat <ofekefrat.w@gmail.com>
2022-03-02 15:28:43 +0200

    Horrible

--- src/Knight.java
+++ src/Knight.java
@@ -8,36 +8,193 @@ import java.util.Scanner;
 public class Knight {
     public static void main(String[] args) {
         //declarations
-        final int BOARD_EDGE = 8;
-
+        final int EDGE_1 = 1;
+        final int EDGE_2 = 8;
+        final int NEAR_EDGE_1 = EDGE_1 + 1;
+        final int NEAR_EDGE_2 = EDGE_2 - 1;
         Scanner scan = new Scanner(System.in);//scanner object
 
-        //asking user for input
+        //describing program to user
         System.out.println("This program reads two integers which " +
                 "represent the knight's location on the chess board: ");
+        //asking user for input
         System.out.println("Please enter the number of row");
         int row = scan.nextInt();//row input
         System.out.println("Please enter the number of column");
         int col = scan.nextInt();//column input
 
-        //Making sure input is legal
-        if((col >= 1 && col <= 8) && (row >= 1 && row <= 8)) {
-            //dividing to scenarios
+        if ((col >= EDGE_1 && col <= EDGE_2) && (row >= EDGE_1 && row <= EDGE_2)) {//legal input check
+            System.out.println("Moves:");//this line should be printed as long as the input is legal
 
-            //scenario 1: all moves can be made
-            if ((col < 7 && col > 2) && (row < 7 && row > 2)) {
-                System.out.println("Moves:");
-                System.out.println((row + 1) + (col + 2));
-                System.out.println((row + 2) + (col + 1));
-                System.out.println((row - 1) + (col + 2));
-                System.out.println((row - 2) + (col + 1));
-                System.out.println((row - 2) + (col - 1));
-                System.out.println((row - 1) + (col - 2));
-                System.out.println((row + 1) + (col - 2));
-                System.out.println((row + 2) + (col - 1));
-            }//end of scenario 1
+            if (col < NEAR_EDGE_2 && col > NEAR_EDGE_1) { //far from horizontal edges
+                if (row < NEAR_EDGE_2 && row > NEAR_EDGE_1) { //far from diagonal edges
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == NEAR_EDGE_1) { //close to top edge
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == EDGE_1) { //at top edge
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == NEAR_EDGE_2) { //close to bottom edge
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                }
+                else { // (row == EDGE_2), at bottom edge
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                }
+            } //end of "far from horizontal edges" scenarios
+
+            else if (row < NEAR_EDGE_2 && row > NEAR_EDGE_1) { // far from diagonal edges
+                if (col == NEAR_EDGE_1) { // close to left edge
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (col == EDGE_1) { // at left edge
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                }
+                else if (col == NEAR_EDGE_2) { // close to right edge
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else {//(col == EDGE_2), at right edge
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+            }//end of "far from diagonal edges" scenarios
+
+            //special cases
+            else if (col == EDGE_1) { // 1st column special cases
+                if (row == NEAR_EDGE_1) { // 2,1
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                }
+                else if (row == EDGE_1) { // 1,1
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                }
+                else if (row == NEAR_EDGE_2) { // 7,1
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                }
+                else { // 8,1
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                }
+            } // end of 1st column special cases
+
+            else if (col == NEAR_EDGE_1) { // 2nd column special cases
+                if (row == NEAR_EDGE_1) { // 2,2
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == EDGE_1) { // 1,2
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == NEAR_EDGE_2) { // 7,2
+                    System.out.println((row + 1) + (col + 2));
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                }
+                else { // 8,2
+                    System.out.println((row - 1) + (col + 2));
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                }
+            } // end of 2nd column special cases
+
+            else if (col == NEAR_EDGE_2) { // 7th column special cases
+                if (row == NEAR_EDGE_1) { // 2,7
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == EDGE_1) { // 1,7
+                    System.out.println((row + 2) + (col + 1));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == NEAR_EDGE_2) { // 7,7
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                }
+                else { // 8,7
+                    System.out.println((row - 2) + (col + 1));
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                }
+            } // end of 7th column special cases
+
+            else { // 8th column special cases
+                if (row == NEAR_EDGE_1) { // 2,8
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == EDGE_1) { // 1,8
+                    System.out.println((row + 1) + (col - 2));
+                    System.out.println((row + 2) + (col - 1));
+                }
+                else if (row == NEAR_EDGE_2) { // 7,8
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                    System.out.println((row + 1) + (col - 2));
+                }
+                else { // 8,8
+                    System.out.println((row - 2) + (col - 1));
+                    System.out.println((row - 1) + (col - 2));
+                }
+            } // end of 8th column special cases
+
         }//end of legal input check
         else
             System.out.println("input is illegal");//if input is illegal, print error message and exit the program
+
     }//end of method main
+
 }//end of class Knight
